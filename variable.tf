c850bd38d87910efefd62db491dd75cae92a7903
Rafael Reynoso <rafaelreynosob@Edwards-MacBook-Air.local>
2023-04-24 22:21:54 -0400

    removing variables

--- variable.tf
+++ variable.tf
@@ -1,15 +1,1 @@
 
-#variable "user1"{
- #   type = string
-  #  sensitive   = true
-   # description = "Username used to log into the Palo Alto firewall"
-#}
-
-#variable "user1_pass"{
- #   type = string
-  #  sensitive   = true
-   # description = "Password for User1"
-#}
-
-
-
